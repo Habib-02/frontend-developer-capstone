fd7b8ef6d46f1a42f70685d6c530501c96a9949e
Habib <moh.habibur.7@gmail.com>
2025-07-21 22:04:08 +0300

    complete Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import "./App.css";
 import Hero from "./components/hero/Helor";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className="main-content">
         <Hero />
+        <Menu />
       </main>
       <Footer />
     </div>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,11 @@
+import styles from "./Menu.module.css";
+
+function Menu() {
+  return (
+    <div className={styles.menu}>
+      <h1>This weeks special</h1>;
+    </div>
+  );
+}
+
+export default Menu;
